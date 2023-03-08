0207730a8d226697b699679fe88611d832b20dbb
Yerin Kim <90273263+Wise-99@users.noreply.github.com>
2023-03-08 19:47:07 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(int n) {
-        int answer = n;
+        int answer = n; // answer에 n값을 넣어 비교
         
         int n_cnt = ToBinary(n); // 주어진 수 n을 이진수로 변환했을 때 1의 갯수
         int big_cnt = 0; // 다음 큰 숫자를 찾기 위해 1의 갯수를 받을 변수
-        while(true){
-            n++;
-            big_cnt = ToBinary(++answer);
+        
+        while(true){ // 다음 큰 숫자를 찾을 때까지 반복
+            big_cnt = ToBinary(++answer); // 먼저 answer를 ++ 한 다음 이진수 변환
             
-            if(n_cnt == big_cnt)
+            if(n_cnt == big_cnt) // 1의 갯수가 같으면 while 종료
                 break;
         }
         
@@ -16,6 +16,7 @@ class Solution {
         
     }
     
+    // 이진수 변환 및 1의 갯수 반환 메소드
     public static int ToBinary(int num){
         String bin = Integer.toBinaryString(num); // 이진수 변환
         int count = 0;
