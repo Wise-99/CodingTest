754e75d4e48683f2f53af1c990ee15bb41c8702d
Yerin Kim <90273263+Wise-99@users.noreply.github.com>
2023-03-24 18:00:37 +0900

    [level 1] Title: 폰켓몬, Time: 4.75 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int pick = nums.length / 2;
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int n : nums){
+            set.add(n);
+        }
+        
+        if(pick >= set.size())
+            answer = set.size();
+        else
+            answer = pick;
+        
+        return answer;
+    }
+}
