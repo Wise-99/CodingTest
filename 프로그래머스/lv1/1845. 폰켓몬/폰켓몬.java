a7970d62c33dee170bf357abccbbd4f9b779341b
Yerin Kim <90273263+Wise-99@users.noreply.github.com>
2023-03-31 19:21:36 +0900

    [level 1] Title: 폰켓몬, Time: 2.81 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -2,19 +2,19 @@ import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        int pick = nums.length / 2;
+
+        int pick = nums.length / 2; // 선택할 수 있는 포켓몬의 갯수
         
         HashSet<Integer> set = new HashSet<>();
-        for(int n : nums){
+        for(int n : nums){ // 해시셋에 모든 포켓몬 추가(중복은 들어가지 않음)
             set.add(n);
         }
         
-        if(pick >= set.size())
-            answer = set.size();
-        else
-            answer = pick;
-        
-        return answer;
+        // (선택할 수 있는 폰켓몬의 수 >= 폰켓몬 종류의 수) 라면
+        // 답은 폰캣몬 종류의 수
+        if(pick >= set.size()) 
+            return set.size();
+        else // 아니라면 선택할 수 있는 폰켓몬의 수
+            return pick;
     }
 }
