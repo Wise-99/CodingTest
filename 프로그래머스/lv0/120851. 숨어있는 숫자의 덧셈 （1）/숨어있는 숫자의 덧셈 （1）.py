8aa4343e947b6e0f4e244150230972216e1fd001
Yerin Kim <90273263+Wise-99@users.noreply.github.com>
2023-04-15 20:27:38 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = []
+    
+    for i in my_string:
+        try:
+            answer.append(int(i))
+        except:
+            continue
+    
+    return sum(answer)
