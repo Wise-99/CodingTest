08f145101d254284be6a050bdd8da5deadcd27d6
Yerin Kim <90273263+Wise-99@users.noreply.github.com>
2023-05-31 18:23:45 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.11 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(t: String, p: String): Int {
+        var answer: Int = 0
+        
+        for(i in 0 .. t.length - p.length){
+
+            if(t.substring(i, i+p.length).toLong() <= p.toLong()){
+                answer ++
+            }
+        }
+        
+        return answer
+    }
+}
